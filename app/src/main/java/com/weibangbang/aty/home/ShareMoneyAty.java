5b21b2d44865239d026b43ebfb1b12423760e2a2
zhangyunfei <32457127@qq.com>
2018-10-10 14:55:51 +0800

    微帮帮布局搭建

--- app/src/main/java/com/weibangbang/aty/home/ShareMoneyAty.java
+++ app/src/main/java/com/weibangbang/aty/home/ShareMoneyAty.java
@@ -1,6 +1,18 @@
 package com.weibangbang.aty.home;
 
+import android.annotation.SuppressLint;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextUtils;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.weibangbang.R;
 import com.weibangbang.base.BaseActivity;
@@ -10,26 +22,50 @@ import com.weibangbang.base.BaseActivity;
  * 创建时间：2018/9/25 15:57
  * 功能描述：分享赚钱
  */
-public class ShareMoneyAty extends BaseActivity{
-    private TextView commit_tv;
+public class ShareMoneyAty extends BaseActivity {
+    private TextView joinTip_tv, invitation_code_tv;
+
     @Override
     public int getLayoutId() {
         return R.layout.acvity_share_money;
     }
 
+    @SuppressLint("ResourceAsColor")
     @Override
     public void initView() {
-        setTitleBar(getResources().getString(R.string.fenxiangzhuanqian),true);
-        commit_tv=findViewById(R.id.commit_tv);
-//        LinearLayout.LayoutParams params=new LinearLayout.LayoutParams(DisplayHelper.dp2px(this,100), ViewGroup.LayoutParams.WRAP_CONTENT);
-//        params.setMargins(0,DisplayHelper.dp2px(this,20),0,0);
-//        params.gravity= Gravity.CENTER_HORIZONTAL;
-//        commit_tv.setLayoutParams(params);
-        commit_tv.setText(R.string.share_money_button);
+        setTitleBar(getResources().getString(R.string.fenxiangzhuanqian), true);
+        joinTip_tv = findViewById(R.id.joinTip_tv);
+        invitation_code_tv = findViewById(R.id.invitation_code_tv);
+
+        SpannableString spannableString = new SpannableString(getResources().getString(R.string.join_tip));
+        StyleSpan styleSpan = new StyleSpan(Typeface.BOLD);
+        spannableString.setSpan(styleSpan, 4, 7, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new ForegroundColorSpan(Color.parseColor("#F9B93D")), 4, 7, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        joinTip_tv.setText(spannableString);
+        invitation_code_tv.setText("kjdksaj99");
     }
 
     @Override
     public void initData() {
+
+    }
+
+    /**
+     * 点击分享按钮
+     */
+    public void onCommit(View view) {
+    }
+
+    /**
+     * 复制按钮
+     */
+    public void onCopy(View view) {
+        if (!TextUtils.isEmpty(invitation_code_tv.getText())) {
+            ClipboardManager clipboardManager = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+            ClipData clipData = ClipData.newPlainText("", invitation_code_tv.getText());
+            clipboardManager.setPrimaryClip(clipData);
+            Toast.makeText(this, "复制成功", Toast.LENGTH_SHORT).show();
+        }
 
     }
 }
